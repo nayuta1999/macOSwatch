d832bda86f8681110158fa26e1f192c8e4ee87d4
nayuta1999 <youmu331@gmail.com>
2018-11-10 16:26:33 +0900

    ディレクトリ作成

---
+++ macOSClock/macOSClock/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macOSClock
+//
+//  Created by horikoshi ryoga on 2018/11/10.
+//  Copyright © 2018年 Ryoga.Horikoshi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macOSClock/macOSClock/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOSClock
+//
+//  Created by horikoshi ryoga on 2018/11/10.
+//  Copyright © 2018年 Ryoga.Horikoshi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
